2e36ea50e0a292e45b36add2a7ab7e8110f13394
95hanho <hosung6674@naver.com>
2025-02-13 21:49:07 +0900

    set_bg중

---
+++ src/main/java/me/_hanho/han_hoseong/controller/UiController.java
@@ -0,0 +1,46 @@
+package me._hanho.han_hoseong.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me._hanho.han_hoseong.model.Background;
+import me._hanho.han_hoseong.model.Parent_menu;
+
+@RestController
+@RequestMapping("/api/ui")
+public class UiController {
+
+	private static final Logger logger = LoggerFactory.getLogger(UiController.class);
+	
+	// 메뉴가져오기
+	@GetMapping("/background")
+	public ResponseEntity<Map<String, Object>> get_bgInfo() {
+		logger.info("get_bgInfo");
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		result.put("msg", "SUCCESS");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+	// 메뉴가져오기
+	@PostMapping("/background")
+	public ResponseEntity<Map<String, Object>> set_bgInfo(@ModelAttribute Background background) {
+		logger.info("set_bgInfo");
+		Map<String, Object> result = new HashMap<String, Object>();
+		
+		System.out.println(background);
+		
+		result.put("msg", "SUCCESS");
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+}
---
+++ src/main/java/me/_hanho/han_hoseong/model/Background.java
@@ -0,0 +1,12 @@
+package me._hanho.han_hoseong.model;
+
+public class Background {
+
+	static public String color;
+	static public String size;
+	static public String position;
+	static public String repeat;
+	static public String image;
+	
+	
+}
